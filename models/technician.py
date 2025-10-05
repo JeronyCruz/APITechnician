320d4a9169de40455d3d6797e38f5e0e102df6af
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-10-05 13:40:08 -0400

    corrections in entity technician

--- models/technician.py
+++ models/technician.py
@@ -12,12 +12,13 @@ class TechnicianResponse(SQLModel):
 class TechnicianBase(SQLModel):
     name: str = Field(index=True, max_length= 50)
     time: int | None = Field(default=None)
+    technicianTypeId: Optional[int] = Field(default=None, foreign_key="tipoTecnico.technicianTypeId")
      
 
 class Technician(TechnicianBase, table =True):
     __tablename__ = "technician"
     technicianId: int | None = Field(default=None, primary_key=True)
-    technicianTypeId: Optional[int] = Field(default=None, foreign_key="tipoTecnico.technicianTypeId")
+    
     tipo: Optional["TechnicianType"] = Relationship(back_populates="tecnicos")
 
 
