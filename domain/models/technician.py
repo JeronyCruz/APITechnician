9d4257293f3bd2ccc9e7095a33c3eb817327bce3
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-10-19 22:37:52 -0400

    update technician

---
+++ domain/models/technician.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, Relationship
+
+class TechnicianBase(SQLModel):
+    name: str = Field(index=True, max_length= 50)
+    time: int | None = Field(default=None)
+    technicianTypeId: Optional[int] = Field(default=None, foreign_key="tipoTecnico.technicianTypeId")
+     
+
+class Technician(SQLModel, table =True):
+    __tablename__ = "technician"
+    technicianId: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True, max_length=50)
+    time: int | None = Field(default=None)
+    technicianTypeId: Optional[int] = Field(default=None,foreign_key="tipoTecnico.technicianTypeId")
+    
+    tipo: Optional["TechnicianType"] = Relationship(back_populates="tecnicos")
+
+class TechnicianResponse(SQLModel):
+    # Este será el orden en el JSON
+    technicianId: int
+    name: str  
+    time: Optional[float]
+    technicianTypeId: Optional[int]
