e36244773d965f01eed16064fce5859d16a6d13f
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-10-23 22:36:31 -0400

    add functions in main to create tables.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from api import technicianRouter, technicianTypeRouter
+from api import technicianRouter, technicianTypeRouter,clientRouter
+from Connection import create_db_and_tables
 
 app = FastAPI()
 app.include_router(technicianRouter.router)
 app.include_router(technicianTypeRouter.router)
+app.include_router(clientRouter.router)
 
-
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/", include_in_schema=False)
 def root():
